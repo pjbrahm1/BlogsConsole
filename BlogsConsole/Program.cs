2b9b2eca42d780071f0be9c245486981697936ff
pjbrahm <pbrahm1@my.wctc.edu>
2019-04-03 19:06:17 -0500

    Add validation

--- BlogsConsole/Program.cs
+++ BlogsConsole/Program.cs
@@ -10,44 +10,58 @@ namespace BlogsConsole
         private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         public static void Main(string[] args)
         {
-            logger.Info("Program started");
+            logger.Info("Program started ");
             string choice = "";
+            bool valid = false;
             do {
 
                 try
                 {
                     // Give user a choice
-                    Console.WriteLine("\nWhat would you like to do? ");
-                    Console.Write("\n(1) Display all blogs (2) Add a blog (3) Create a post (4) Display posts: ");
+                    Console.WriteLine("\nEnter your selection: ");
+                    Console.Write("\n1) Display all blogs \n2) Add a blog \n3) Create a post \n4) Display posts");
+                    Console.WriteLine("\nEnter q to quit");
                     choice = Console.ReadLine();
+                    Console.Clear();
+                    logger.Info("Option " + choice + " selected");
 
                     if (choice == "1")
                     {
                         // Display all Blogs and Posts from the database
                         var db = new BloggingContext();
                         var query = db.Blogs.OrderBy(b => b.BlogId);
+                        var total = db.Blogs.Count();
+                        Console.WriteLine("\n" + total + " Blogs displayed ");
 
-                        Console.WriteLine("\nAll blogs in the database:");
                         Console.WriteLine();
                         foreach (var item in query)
                         {
-                            Console.WriteLine(item.BlogId + ")\t" + item.Name);
+                            Console.WriteLine("  " + item.Name);
                         }
-                        Console.WriteLine();
-                        logger.Info("\nBlogs displayed ");
+
                     }
                     else if (choice == "2")
                     {
                         // Create and save a new Blog
                         Console.Write("\nEnter a name for a new Blog: ");
                         var name = Console.ReadLine();
+                        while (name.Length == 0)
+                        {
+                            Console.WriteLine("Blog name cannot be blank");
+                            Console.Write("\nEnter a name for a new Blog or q to quit: ");
+                            name = Console.ReadLine();
+                        }
+                        if (name == "q" || name == "Q")
+                        { }
+                        else
+                        {
+                            var blog = new Blog { Name = name };
 
-                        var blog = new Blog { Name = name };
-
-                        var db = new BloggingContext();
-                        db.AddBlog(blog);
-                        Console.WriteLine();
-                        logger.Info("\nBlog added - {name}", name);
+                            var db = new BloggingContext();
+                            db.AddBlog(blog);
+                            Console.WriteLine();
+                            logger.Info("\nBlog added - {name}", name);
+                        }
                     }
                     else if (choice == "3")
                     {
@@ -58,7 +72,7 @@ namespace BlogsConsole
                         Console.WriteLine();
                         foreach (var item in query)
                         {
-                            Console.WriteLine(item.BlogId + ")\t " + item.Name);
+                            Console.WriteLine(item.BlogId + ") " + item.Name);
                         }
                         // Select the Blog
                         Console.Write("\nSelect the id of the blog you want to post to? ");
@@ -66,21 +80,53 @@ namespace BlogsConsole
                         var blogCntxtDb = new BloggingContext();
                         var name = blogCntxtDb.Blogs.Where(b => b.BlogId.Equals(blogId));
 
-                        foreach (var thing in name)
+                        foreach (var item in query)
                         {
-                            Console.Write("\nTitle your post for " + thing.Name + ": ");
-                            var postTitle = Console.ReadLine();
-
-                            Console.Write("\nEnter your post to " + postTitle + ": ");
-                            var postContent = Console.ReadLine();
-
-                            var postDb = new Post { Title = postTitle, Content = postContent, BlogId = thing.BlogId };
-
-                            var db = new BloggingContext();
-                            db.AddPost(postDb);
+                            if (item.BlogId == blogId)
+                            {
+                                valid = true;
+                            }
                         }
-                        Console.WriteLine();
-                        logger.Info("\nPost added - {name}", name);
+
+                        if (!valid)
+                        {
+                            Console.WriteLine("You have made an invalid selection");
+                        }
+                        else
+                        {
+                            foreach (var thing in name)
+                            {
+                                Console.Write("\nTitle your post for " + thing.Name + ": ");
+                                var postTitle = Console.ReadLine();
+                                while (postTitle.Length == 0)
+                                {
+                                    Console.WriteLine("Post title can not be blank");
+                                    Console.Write("\nTitle your post for " + thing.Name + " or enter q to quit: ");
+                                    postTitle = Console.ReadLine();
+                                }
+                                if (postTitle == "q" || postTitle == "Q")
+                                {
+                                }
+                                else
+                                {
+                                    Console.Write("\nEnter your post to " + postTitle + ": ");
+                                    var postContent = Console.ReadLine();
+                                    while (postContent.Length == 0)
+                                    {
+                                        Console.WriteLine("Post content can not be blank");
+                                        Console.Write("\nEnter your post to " + postTitle + ":");
+                                        postContent = Console.ReadLine();
+                                    }
+                                    var postDb = new Post { Title = postTitle, Content = postContent, BlogId = thing.BlogId };
+
+                                    var db = new BloggingContext();
+                                    db.AddPost(postDb);
+
+                                    logger.Info("\nPost added - {postTitle}", postTitle);
+                                    Console.WriteLine();
+                                }
+                            }
+                        }
                     }
                     else if (choice == "4")
                     {
@@ -91,44 +137,61 @@ namespace BlogsConsole
 
                         foreach (var item in query)
                         {
-                            Console.WriteLine(item.BlogId + ")\t Posts from " + item.Name);
+                            Console.WriteLine(item.BlogId + ") Posts from " + item.Name);
                         }
 
                         // Select the Blog
-                        Console.Write("\nSelect the blog's post to display: ");
+                        Console.Write("\nSelect the blog's posts to display: ");
                         int blogId = int.Parse(Console.ReadLine());
                         Console.WriteLine();
-
-                        var blogCntxtDb = new BloggingContext();
-                        var name = blogCntxtDb.Blogs.Where(b => b.BlogId.Equals(blogId));
-
-                        // Display all Blogs from the database
-                        var showPostDb = new BloggingContext();
-                        var blogger = showPostDb.Posts.Where(b => b.BlogId.Equals(blogId));
-                        foreach (var thing in name)
+                       
+                        foreach (var item in query)
                         {
-                            Console.WriteLine("\nBlog: " + thing.Name);
+                            if (item.BlogId == blogId)
+                            { valid = true; }
                         }
-                        foreach (var posts in blogger)
+
+                        if (!valid)
                         {
-                            Console.WriteLine("\n      Title: " + posts.Title);
-                            Console.WriteLine("\n             Content: " + posts.Content);
+                            Console.WriteLine("You have made an invalid selection");
                         }
-                        Console.WriteLine();
-                        logger.Info("\nPosts displayed - {name}", name);
+                        else
+                        {
+                            var blogCntxtDb = new BloggingContext();
+                            var name = blogCntxtDb.Blogs.Where(b => b.BlogId.Equals(blogId));
+
+                            // Display all posts from the database
+                            var showPostDb = new BloggingContext();
+                            var blogger = showPostDb.Posts.Where(b => b.BlogId.Equals(blogId));
+                            var total = blogger.Count();
+                            Console.WriteLine("\n" + total + " Posts displayed ");
+
+                            foreach (var thing in name)
+                            {
+                                Console.WriteLine("\nBlog: " + thing.Name);
+                            }
+                            foreach (var posts in blogger)
+                            {
+                                Console.WriteLine("\n      Title: " + posts.Title);
+                                Console.WriteLine("\n             Content: " + posts.Content);
+                            }
+                            Console.WriteLine();
+                            foreach (var thing in name)
+                            {
+                                logger.Info("\nPosts displayed - {name}", thing.Name);
+                            }
+                        }
+                    } else
+                    {
+                        Console.WriteLine("Invalid selection. Choose options 1 - 4 or q to quit");
                     }
-                }
+                }  
                 catch (Exception ex)
                 {
                     logger.Error(ex.Message);
                 }
-            } while (choice != "");
-            // Give error message
-            Console.Write("You've made an invalid selection. ");
-
-            Console.WriteLine("Press enter to quit");
-            string x = Console.ReadLine();
-
+            } while (choice != "q" && choice != "Q");
+    
             logger.Info("Program ended");
         }
     }
